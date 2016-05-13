5c0566ca14daab6b7b902cc83c06374bfd7ec124
ADDAUI <addaui@outlook.com>
2016-05-13 05:59:33 +0300

    Used Lambda in Controller class.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,9 +1,5 @@
 package sample;
 
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
@@ -19,7 +15,7 @@ public class Controller implements Initializable {
     private static final String logID = "Dev_ADDAUI";
 
     private NotesList notesList = new NotesList();
-    private TreeItem<Note> rootItem = new TreeItem<Note>(new Note("Quick Notes", ""));
+    private TreeItem<Note> rootItem = new TreeItem<>(new Note("Quick Notes", ""));
 
     //Defining Gui Elements
     @FXML Label notification;
@@ -49,68 +45,56 @@ public class Controller implements Initializable {
             note_panel.setDisable(false);
         }
 
-        newBtn.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                //TODO flash a notification if created, and maybe save the current note before creating a new one
-                saveBtn.fire();
-                notesList.newNote("New Note", "");
-                TreeItem<Note> item = new TreeItem<Note>(notesList.getNote(notesList.getSize() - 1));
-                rootItem.getChildren().add(item);
-                tree.getSelectionModel().select(item);
-                Log.i(logID, "\n" + notesList.toString());
-            }
+        newBtn.setOnAction(event -> {
+            //TODO flash a notification if created, and maybe save the current note before creating a new one
+            saveBtn.fire();
+            notesList.newNote("New Note", "");
+            TreeItem<Note> item = new TreeItem<>(notesList.getNote(notesList.getSize() - 1));
+            rootItem.getChildren().add(item);
+            tree.getSelectionModel().select(item);
+            Log.i(logID, "\n" + notesList.toString());
         });
 
-        saveBtn.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                //TODO flash a notification if saved
-                if(!tree.getSelectionModel().isEmpty()) {
-                    String newTitle = title.getText();
-                    String newContent = content.getText();
-                    notesList.getNote(tree.getSelectionModel().getSelectedIndex()).updateNote(newTitle, newContent, Date.from(Instant.now()));
-                    tree.refresh();
-                }
-            }
-        });
-
-        deleteBtn.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                //TODO Flash a notification if the tree empty
-                if (rootItem.getChildren().size() == 0) {
-                    Log.w(logID, "Hey the tree is empty, stop pushing!!");
-                    return;
-                }
-                int index = tree.getSelectionModel().getSelectedIndex();
-                notesList.deleteNote(index);
-                rootItem.getChildren().remove(index);
-                if (rootItem.getChildren().size() == 0) {
-                    note_panel.setDisable(true);
-                }
+        saveBtn.setOnAction(event -> {
+            //TODO flash a notification if saved
+            if(!tree.getSelectionModel().isEmpty()) {
+                String newTitle = title.getText();
+                String newContent = content.getText();
+                notesList.getNote(tree.getSelectionModel().getSelectedIndex()).updateNote(newTitle, newContent, Date.from(Instant.now()));
                 tree.refresh();
             }
         });
 
-        tree.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<TreeItem<Note>>() {
-            @Override
-            public void changed(ObservableValue<? extends TreeItem<Note>> observable, TreeItem<Note> oldValue, TreeItem<Note> newValue) {
-                Log.v(logID, "Controller.changed");
-                Log.v(logID, "observable = [" + observable + "], oldValue = [" + oldValue + "], newValue = [" + newValue + "]");
-
-                if (newValue != null) {
-                    updateDetails(newValue.getValue());
-                }
-
-                note_panel.setDisable(false);
+        deleteBtn.setOnAction(event -> {
+            //TODO Flash a notification if the tree empty
+            if (rootItem.getChildren().size() == 0) {
+                Log.w(logID, "Hey the tree is empty, stop pushing!!");
+                return;
             }
+            int index = tree.getSelectionModel().getSelectedIndex();
+            notesList.deleteNote(index);
+            rootItem.getChildren().remove(index);
+            if (rootItem.getChildren().size() == 0) {
+                note_panel.setDisable(true);
+            }
+            tree.refresh();
+        });
+
+        tree.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+            Log.v(logID, "Controller.changed");
+            Log.v(logID, "observable = [" + observable + "], oldValue = [" + oldValue + "], newValue = [" + newValue + "]");
+
+            if (newValue != null) {
+                updateDetails(newValue.getValue());
+            }
+
+            note_panel.setDisable(false);
         });
 
 
     }
 
-    public void updateDetails(Note note) {
+    private void updateDetails(Note note) {
 
         title.setText(note.getTitle());
         content.setText(note.getContent());

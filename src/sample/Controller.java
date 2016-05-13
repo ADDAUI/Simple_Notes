058987ba8ac0399fe06e106de0deaa8c41573883
ADDAUI <addaui@outlook.com>
2016-05-13 07:29:55 +0300

    - Added a Notification Panel . - Added animation to the notification. - Code organized.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,9 +1,13 @@
 package sample;
 
+import javafx.animation.FadeTransition;
+import javafx.beans.property.ObjectProperty;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+import javafx.util.Duration;
 
 import java.net.URL;
 import java.time.Instant;
@@ -12,8 +16,10 @@ import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
 
+    //LogID and sample.Log class are used only for logging purposes.
     private static final String logID = "Dev_ADDAUI";
 
+    //notesList are the note list.
     private NotesList notesList = new NotesList();
     private TreeItem<Note> rootItem = new TreeItem<>(new Note("Quick Notes", ""));
 
@@ -30,6 +36,10 @@ public class Controller implements Initializable {
     @FXML Label date_updated;
     @FXML Label time_updated;
     @FXML AnchorPane note_panel;
+    @FXML HBox notification_panel;
+
+    //animation
+    private FadeTransition fadeIn = new FadeTransition(new Duration(2000));
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -46,37 +56,50 @@ public class Controller implements Initializable {
         }
 
         newBtn.setOnAction(event -> {
-            //TODO flash a notification if created, and maybe save the current note before creating a new one
-            saveBtn.fire();
+            if(!tree.getSelectionModel().isEmpty()) {
+                saveBtn.fire();
+            }
             notesList.newNote("New Note", "");
             TreeItem<Note> item = new TreeItem<>(notesList.getNote(notesList.getSize() - 1));
             rootItem.getChildren().add(item);
             tree.getSelectionModel().select(item);
+            setNotification("New Note Created");
             Log.i(logID, "\n" + notesList.toString());
         });
 
-        saveBtn.setOnAction(event -> {
-            //TODO flash a notification if saved
+        saveBtn.setOnAction(event -> {if (rootItem.getChildren().size() == 0) {
+                Log.w(logID, "Hey the tree is empty!!");
+                setNotification("No Note Selected !!!");
+                return;
+            }
             if(!tree.getSelectionModel().isEmpty()) {
                 String newTitle = title.getText();
                 String newContent = content.getText();
                 notesList.getNote(tree.getSelectionModel().getSelectedIndex()).updateNote(newTitle, newContent, Date.from(Instant.now()));
                 tree.refresh();
+                setNotification("Note Saved");
             }
         });
 
         deleteBtn.setOnAction(event -> {
-            //TODO Flash a notification if the tree empty
             if (rootItem.getChildren().size() == 0) {
-                Log.w(logID, "Hey the tree is empty, stop pushing!!");
+                Log.w(logID, "Hey the tree is empty!!");
+                setNotification("No Note Selected!");
                 return;
             }
             int index = tree.getSelectionModel().getSelectedIndex();
             notesList.deleteNote(index);
             rootItem.getChildren().remove(index);
             if (rootItem.getChildren().size() == 0) {
+                title.clear();
+                content.clear();
+                date_created.setText("00/00/0000");
+                date_updated.setText("00/00/0000");
+                time_created.setText("00:00:00 AM");
+                time_updated.setText("00:00:00 AM");
                 note_panel.setDisable(true);
             }
+            setNotification("Note Deleted");
             tree.refresh();
         });
 
@@ -98,11 +121,23 @@ public class Controller implements Initializable {
 
         title.setText(note.getTitle());
         content.setText(note.getContent());
+        //TODO http://stackoverflow.com/questions/657962/a-non-deprecated-exact-equivalent-of-datestring-s-in-java
         date_created.setText(note.getCreated().toLocaleString().substring(0, 12));
         time_created.setText(note.getCreated().toLocaleString().substring(13, 23));
         date_updated.setText(note.getUpdated().toLocaleString().substring(0, 12));
         time_updated.setText(note.getUpdated().toLocaleString().substring(13, 23));
 
 
+    }
+
+    private void setNotification(String message){
+        notification.setText(message);
+        fadeIn.setNode(notification_panel);
+        fadeIn.setFromValue(0.00);
+        fadeIn.setToValue(1);
+        fadeIn.setCycleCount(2);
+        fadeIn.setAutoReverse(true);
+        fadeIn.playFromStart();
+        fadeIn.setOnFinished(value -> notification.setText(""));
     }
 }
